06fffcf61163442542f1bc47ebf59295b34541f8
Thomas Laforge <thomas.laforge.38@gmail.com>
2018-12-03 10:06:49 +0100

    server on class

--- src/server/index.ts
+++ src/server/index.ts
@@ -20,10 +20,6 @@ import { GameCollection } from '../common/modules/GameCollection';
 
 import { serverPort } from '../common/modules/Server';
 
-const app = express()
-const http = new nodeHttp.Server(app);
-const io = socketIo(http);
-let GC = new GameCollection()
 const games: any = [
     {
         name: 'limitelimite',
@@ -39,79 +35,98 @@ const games: any = [
     }
 ]
 
-let hasLoggedAllSocketEvents = false
+export class BoardGameServer {
 
-// render index page
-// console.log('dirName =', __dirname, path.join(__dirname, '../../src/client'))
-const clientDirPath = path.join(__dirname, '../../src/client')
+    public games: any[]
 
-// public directory
-app.use(express.static(clientDirPath));
-
-// render app
-app.get('/', function (req, res) {
-    res.sendFile(path.join(clientDirPath, 'index.html'));
-});
-
-// INFO: naming of "on" events and emit: Category:portée.main_mission/sub_mision_or_state
-// ex: on Lobby:create_game/succes
-// ex: emit Lobby:player.create_game/succes
-
-io.on('connection', (baseSocket: ExtendedSocket) => {
-    const server = new SocketIoDescriptor(baseSocket.server)
-    let socket = new SuperSocket(baseSocket)
-    console.log('a user connected', socket.id);
-
-    // add events from modules
-    addLoginEvents(socket)
-    addChatEvents(socket)
-    addLobbyEvents(socket, GC)
-
-    // add games events
-    games.forEach( (g: any) => {
-        g.addEvents(socket, GC)
-    })
-
-    socket.on('reconnecting', () => {
-        console.log('reconnecting...')
-    })
-
-    socket.on('reconnect', () => {
-        console.log('reconnect...')
-    })
-
-    socket.on('disconnect', () => {
-        let game = GC.getGameWithUser(socket.id)
-        if (game) {
-            let gameHasStarted = game.startGameDate
-            if (gameHasStarted) {
-                // delete the game
-                // GC.removeGame(game.id)
-            }
-            else {
-                // remove player
-                game.removePlayer(socket.id)
-            }
-            socket.baseSocket.to(game.id).emit('game:user_disconnect', socket.username, gameHasStarted)
-        }
-    })
-
-    // Debugs
-    socket.on('debug:get_all_sockets', () => {
-        console.log('ask getAllSockets', server.allUsernames)
-        io.clients((err: any, clients: any[]) => {
-            socket.emit('debug:get_all_sockets', clients)
-        })
-    })
-
-    // logging events
-    if (!hasLoggedAllSocketEvents) {
-        console.log('allEvents', baseSocket.eventNames())
-        hasLoggedAllSocketEvents = true
+    constructor(games: any){
+        this.games = games
     }
-});
 
-// start server
-http.listen(serverPort, function () {
-    console.log('listening on localhost:' + serverPort);
-});
+    start(){
+        const app = express()
+        const http = new nodeHttp.Server(app);
+        const io = socketIo(http);
+        let GC = new GameCollection()
+    
+        let hasLoggedAllSocketEvents = false
+    
+        // render index page
+        // console.log('dirName =', __dirname, path.join(__dirname, '../../src/client'))
+        const clientDirPath = path.join(__dirname, '../../src/client')
+    
+        // public directory
+        app.use(express.static(clientDirPath));
+    
+        // render app
+        app.get('/', function (req, res) {
+            res.sendFile(path.join(clientDirPath, 'index.html'));
+        });
+    
+        // INFO: naming of "on" events and emit: Category:portée.main_mission/sub_mision_or_state
+        // ex: on Lobby:create_game/succes
+        // ex: emit Lobby:player.create_game/succes
+    
+        io.on('connection', (baseSocket: ExtendedSocket) => {
+            const server = new SocketIoDescriptor(baseSocket.server)
+            let socket = new SuperSocket(baseSocket)
+            console.log('a user connected', socket.id);
+    
+            // add events from modules
+            addLoginEvents(socket)
+            addChatEvents(socket)
+            addLobbyEvents(socket, GC)
+    
+            // add games events
+            games.forEach( (g: any) => {
+                g.addEvents(socket, GC)
+            })
+    
+            socket.on('reconnecting', () => {
+                console.log('reconnecting...')
+            })
+    
+            socket.on('reconnect', () => {
+                console.log('reconnect...')
+            })
+    
+            socket.on('disconnect', () => {
+                let game = GC.getGameWithUser(socket.id)
+                if (game) {
+                    let gameHasStarted = game.startGameDate
+                    if (gameHasStarted) {
+                        // delete the game
+                        // GC.removeGame(game.id)
+                    }
+                    else {
+                        // remove player
+                        game.removePlayer(socket.id)
+                    }
+                    socket.baseSocket.to(game.id).emit('game:user_disconnect', socket.username, gameHasStarted)
+                }
+            })
+    
+            // Debugs
+            socket.on('debug:get_all_sockets', () => {
+                console.log('ask getAllSockets', server.allUsernames)
+                io.clients((err: any, clients: any[]) => {
+                    socket.emit('debug:get_all_sockets', clients)
+                })
+            })
+    
+            // logging events
+            if (!hasLoggedAllSocketEvents) {
+                console.log('allEvents', baseSocket.eventNames())
+                hasLoggedAllSocketEvents = true
+            }
+        });
+    
+        // start server
+        http.listen(serverPort, function () {
+            console.log('listening on localhost:' + serverPort);
+        });
+    }
+
+}
+
+new BoardGameServer(games).start()
