5146c84fc0367b5cf649da6cb6a33789883162bb
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-04-18 20:02:20 +0200

    get nb player of tarot congolais by variable

--- src/common/modules/MultiplayerGame.ts
+++ src/common/modules/MultiplayerGame.ts
@@ -1,5 +1,6 @@
 import { Game } from "./Game";
 import { DEFAULT_IS_PRIVATE_GAME, DEFAULT_NB_PLAYER } from "../LimiteLimite/LimiteLimite";
+import { DEFAULT_NB_PLAYER as DEFAULT_NB_PLAYER_TC } from "../tarotcongolais/tarotcongolais";
 import { SocketPlayer } from "./SocketPlayer";
 
 export class MultiplayerGame extends Game {
@@ -20,7 +21,7 @@ export class MultiplayerGame extends Game {
         if(!nbPlayer){
             switch (gameType) {
                 case 'tarotcongolais':
-                    nbPlayer = 4; break;
+                    nbPlayer = DEFAULT_NB_PLAYER_TC; break;
                 default:
                     nbPlayer = DEFAULT_NB_PLAYER; break;
             }
