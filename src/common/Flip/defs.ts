19d20db8c44f862ad64e9cef17fabc7da1ce8c90
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-12 10:39:21 +0200

    add some ui

--- src/common/Flip/defs.ts
+++ src/common/Flip/defs.ts
@@ -4,6 +4,8 @@ export const DEFAULT_NB_PACKS = 2
 export const NB_PLAYER = 2
 export const NB_CARDS_PLAYABLE = 5
 
+export const prefix = 'flip'
+
 export enum CardColor {
     Spade = 0,
     Club = 1,
