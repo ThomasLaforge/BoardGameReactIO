47f9c5915edfbe9aaf5d34a482f07194f1e6008b
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-14 20:08:18 +0200

    test creation of game

---
+++ src/common/Flip/tests/game.test.ts
@@ -0,0 +1,20 @@
+import { Game } from "../Game";
+import { Player } from "../Player";
+import { SocketPlayer } from "../../modules/SocketPlayer";
+
+describe('flip game', () => {
+    
+    let players = [
+        new SocketPlayer('thomas', 'tom_socket'),
+        new SocketPlayer('melane', 'mel_socket')
+    ]
+    let game = new Game(players)
+    
+    test('init game', () => {
+        game.start()
+        game.players.forEach(p => {
+            console.log('decks', p.username, p.deck)
+        })
+        expect(game.players.length).toEqual(2);
+    })
+})
